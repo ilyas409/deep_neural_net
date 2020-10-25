a9327e9bbf657eaad0bc0f4de6e2a400e797843c
Ilyas A <ilyas408@icloud.com>
2020-10-24 22:20:52 -0700

    refactored backprop and got rid of the dictionary

--- model.py
+++ model.py
@@ -34,20 +34,13 @@ class Model:
             self.memory["Z" + str(i)] = Z_curr
             
         return A_curr
-    
-    def update_params(self, grads, learning_rate):
-        for i in range(1, len(self.layers)):
-            self.layers[i].W -= learning_rate * grads["dW" + str(i)]
-            self.layers[i].b -= learning_rate * grads["db" + str(i)]
-    
+        
     def fit(self, X, Y, learning_rate, iterations):
         self.learning_rate = learning_rate
-        grads = {}
         costs = []
         for i in range(iterations):
             Y_hat = self.prop_forward(X)
-            grads = self.propagate_backward(Y_hat, Y)
-            self.update_params(grads, learning_rate)
+            self.propagate_backward(Y_hat, Y)
             cost = self.compute_cost(Y_hat, Y)
             costs.append(cost)
             if i > 10 and i % int(iterations * 0.1) == 0:
@@ -58,9 +51,7 @@ class Model:
         m = AL.shape[1]
         Y = Y.reshape(AL.shape)
         
-        grads_values = {}
         epsilon = 1e-8
-        
         dA_prev = - (np.divide(Y, AL + epsilon) - np.divide(1 - Y, 1 - AL + epsilon))
         
         for i in reversed(range(1, len(self.layers))):
@@ -79,7 +70,5 @@ class Model:
             db_curr = 1/m * np.sum(dZ_curr, axis=1, keepdims=True)
             dA_prev = np.dot(W_curr.T, dZ_curr)
             
-            grads_values["dW" + str(i)] = dW_curr
-            grads_values["db" + str(i)] = db_curr
-            
-        return grads_values
+            self.layers[i].W -= self.learning_rate * dW_curr
+            self.layers[i].b -= self.learning_rate * db_curr
