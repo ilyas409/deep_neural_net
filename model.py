40754b95966f58449e3678c141a46ad867abc171
Ilyas A <ilyas408@icloud.com>
2020-10-24 23:11:28 -0700

    adam optimizer wip

--- model.py
+++ model.py
@@ -2,8 +2,9 @@ import numpy as np
 
 class Model:
     """Sequential binary classifier. Supports any number of `Dense` layers"""
-    def __init__(self, layers):
+    def __init__(self, layers, use_adam_optimizer=True):
         self.layers = layers
+        self.use_adam_optimizer = use_adam_optimizer
         inp_size = self.layers[0].input_size
         # Generate weights and biases:
         for i in range(1, len(self.layers)):
@@ -43,6 +44,32 @@ class Model:
                 print("cost after iteration",i,":",cost)
         return costs
     
+    def update_parameters(self, W, b, dW, db):
+        if not self.use_adam_optimizer:
+            return W - self.learning_rate * dW, b - self.learning_rate * db
+        
+        # Moving average of the gradients. Inputs: "v, grads, beta1". Output: "v".
+        vdW = beta1 * vdW + (1 - beta1) * dW
+        vdb = beta1 * vdb + (1 - beta1) * db
+
+        # Compute bias-corrected first moment estimate. Inputs: "v, beta1, t". Output: "v_corrected".
+        v_corrected_dW = vdW / (1 - beta1)
+        v_corrected_db = vdb / (1 - beta1)
+
+        # Moving average of the squared gradients. Inputs: "s, grads, beta2". Output: "s".
+        sdW = beta2 * sdW + (1 - beta2) * np.power(dW, 2)
+        sdb = beta2 * sdb + (1 - beta2) * np.power(db, 2)
+
+        # Compute bias-corrected second raw moment estimate. Inputs: "s, beta2, t". Output: "s_corrected".
+        s_corrected_dW = sdW / (1 - beta2)
+        s_corrected_db = sdb / (1 - beta2)
+
+        # Update parameters. Inputs: "parameters, learning_rate, v_corrected, s_corrected, epsilon". Output: "parameters".
+        W = W - learning_rate * v_corrected_dW / (np.sqrt(s_corrected_dW + epsilon)
+        b = b - learning_rate * v_corrected_db / (np.sqrt(s_corrected_db + epsilon)
+        
+        return W, b
+    
     def propagate_backward(self, AL, Y):
         m = AL.shape[1]
         Y = Y.reshape(AL.shape)
@@ -66,5 +93,7 @@ class Model:
             db_curr = 1/m * np.sum(dZ_curr, axis=1, keepdims=True)
             dA_prev = np.dot(W_curr.T, dZ_curr)
             
-            self.layers[i].W -= self.learning_rate * dW_curr
-            self.layers[i].b -= self.learning_rate * db_curr
+            self.layers[i].W, self.layers[i] = self.update_parameters(self.layers[i].W,
+                                                                      self.layers[i].b,
+                                                                      dW_curr,
+                                                                      db_curr)
