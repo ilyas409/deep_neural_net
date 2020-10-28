fix: address critical numerical stability and weight initialization issues

layer.py
@@ -24,7 +24,6 @@ class Dense(Layer):
         self.D = None
         
     def init_params(self, inp_size):
-        np.random.seed(1)
         self.W = np.random.randn(self.n_units, inp_size) * self.weight_multiplier
         self.b = np.zeros((self.n_units, 1))
         
@@ -40,3 +39,4 @@ class Dense(Layer):
     
     def __str__(self):
         return "Number of units:" + str(self.n_units) + ", " + str(self.activation)
+    

model.py
@@ -12,7 +12,9 @@ class Model:
     
     def compute_cost(self, AL, Y):
         m = AL.shape[1]
-        cost = - 1/m * np.sum(Y * np.log(AL) + (1 - Y) * np.log(1 - AL), axis=1)
+        epsilon = 1e-8
+        AL_clipped = np.clip(AL, epsilon, 1 - epsilon)
+        cost = - 1/m * np.sum(Y * np.log(AL_clipped) + (1 - Y) * np.log(1 - AL_clipped), axis=0)
         return np.squeeze(cost)
     
     def evaluate(self, X, Y):
@@ -48,7 +50,8 @@ class Model:
         Y = Y.reshape(AL.shape)
         
         epsilon = 1e-8
-        dA_prev = - (np.divide(Y, AL + epsilon) - np.divide(1 - Y, 1 - AL + epsilon))
+        AL_clipped = np.clip(AL, epsilon, 1 - epsilon)
+        dA_prev = - (np.divide(Y, AL_clipped) - np.divide(1 - Y, 1 - AL_clipped))
         
         for i in reversed(range(1, len(self.layers))):
             dA_curr = dA_prev